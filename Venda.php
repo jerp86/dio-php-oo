e1d83851d91f511b4a496aff3e88a17067d70f99
jerp86 <jerp4@hotmail.com>
2020-12-21 10:05:13 -0300

    Acrescentado a descrição do exercício proposto

--- Venda.php
+++ Venda.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * Exercício proposto:
+ * Crie uma classe chamada "Venda" com os atributos privados "data", "produto", "quantidade" e "valorTotal".
+ * Crie um método capaz de exibir os dados da venda. Crie uma instância da classe e passe os atributos através
+ * de um método construtor, em sequiga, invoque o método responsável por exibir o conteúdo da venda devidamente
+ * formatado.
+ */
 class Venda {
   private $data;
   private $produto;
