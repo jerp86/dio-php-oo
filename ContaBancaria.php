b1aef179381b54e9178b4edf70f01704cd7b7e24
jerp86 <jerp4@hotmail.com>
2020-12-20 21:05:22 -0300

    Funcionalidades de métodos

--- ContaBancaria.php
+++ ContaBancaria.php
@@ -1,18 +1,56 @@
 <?php
 class ContaBancaria {
-  public $banco;
-  public $nomeTitular = 'José Eduardo';
-  public $numeroAgencia = '4567';
-  public $numeroConta;
-  public $saldo = 1000.00;
+  private $banco;
+  private $nomeTitular;
+  private $numeroAgencia;
+  private $numeroConta;
+  private $saldo;
+
+  public function __construct($banco, $nomeTitular, $numeroAgencia, $numeroConta, $saldo) {
+    $this->banco = $banco;
+    $this->nomeTitular = $nomeTitular;
+    $this->numeroAgencia = $numeroAgencia;
+    $this->numeroConta = $numeroConta;
+    $this->saldo = $saldo;
+  }
+
+  public function obterSaldo() {
+    return 'Seu saldo atual é: R$ ' . $this->saldo;
+  }
+
+  public function depositar($valor) {
+    $this->saldo += $valor;
+    return 'Depósito de R$ ' . $valor . ' realizado';
+  }
+
+  public function sacar($valor) {
+    $this->saldo -= $valor;
+    return 'Saque de R$ ' . $valor . ' realizado';
+  }
 }
 
-$conta = new ContaBancaria();
+$conta = new ContaBancaria(
+  'Banco do Brasil',              // banco
+  'José Eduardo Rodrigues Pinto', // nomeTitular
+  '8244',                         // numeroAgencia
+  '57354-10',                     // numeroConta
+  0                               // saldo
+);
 
-// var_dump($conta);
-// var_dump($conta->nomeTitular);
-var_dump($conta->saldo);
+echo $conta->obterSaldo(); // 0
 
-$conta->saldo = -500.00;
+echo PHP_EOL;
 
-var_dump($conta->saldo);
+echo $conta->depositar(300.00);
+
+echo PHP_EOL;
+
+echo $conta->obterSaldo(); // 300
+
+echo PHP_EOL;
+
+echo $conta->sacar(150.00);
+
+echo PHP_EOL;
+
+echo $conta->obterSaldo(); // 150
