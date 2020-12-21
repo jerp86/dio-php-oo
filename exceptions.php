1405e677bde4c597c708138f54cd721596f6c55b
jerp86 <jerp4@hotmail.com>
2020-12-21 16:30:13 -0300

    Validando o usuário sem usar Exception

--- exceptions.php
+++ exceptions.php
@@ -1,5 +1,24 @@
 <?php
 
-throw new Exception("Essa é uma exceção!");
+function validarUsuario(array $usuario) {
+    if (empty($usuario['codigo']) || empty($usuario['nome']) || empty($usuario['idade'])) {
+        return false;
+    }
 
-echo '\n... executando ...\n';
+    return true;
+}
+
+$usuario = [
+    'codigo' => 1,
+    'nome' => '',
+    'idade' => 57,
+];
+
+$usuarioValido = validarUsuario(($usuario));
+
+if (!$usuarioValido) {
+    echo "Usuário Inválido!";
+    return false;
+}
+
+echo "\n... executando ...\n";
